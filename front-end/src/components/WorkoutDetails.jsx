436403d615f5ade5c8daaa05ce8f9d1cf2cac63d
Adham Aboelela <100962253+AdhamIsHere@users.noreply.github.com>
2024-07-01 21:47:05 +0300

    added delete workout

--- front-end/src/components/WorkoutDetails.jsx
+++ front-end/src/components/WorkoutDetails.jsx
@@ -1,4 +1,22 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { FaTrash } from "react-icons/fa";
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const res = await fetch(`/api/workout/${workout._id}`, {
+      method: "DELETE",
+    });
+    const json = await res.json();
+    if (!res.ok) {
+      console.log(json.error);
+    } else {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+    console.log(json);
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -10,7 +28,10 @@ const WorkoutDetails = ({ workout }) => {
         <strong>Reps: </strong>
         {workout.reps}
       </p>
-      <p>{workout.createdAt}</p>
+      <p>{new Date(workout.createdAt).toLocaleString()}</p>
+      <span id="deleteSpan">
+        <FaTrash className="deleteIcon" onClick={handleClick} />
+      </span>
     </div>
   );
 };
