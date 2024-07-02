f320bbb4341ba5d384b95550137f05c61a781fe2
Adham Aboelela <100962253+AdhamIsHere@users.noreply.github.com>
2024-07-02 19:30:23 +0300

    chore: Update dependencies and add JWT authentication

--- Back-end/controllers/userController.js
+++ Back-end/controllers/userController.js
@@ -1,5 +1,11 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 // login
 const login = async (req, res) => {
   res.send({ message: "login" });
@@ -7,7 +13,18 @@ const login = async (req, res) => {
 
 // signup
 const signup = async (req, res) => {
-  res.send({ message: "signup" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    // create token
+    const token = generateToken(user._id);
+
+    res.status(201).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = { signup, login };
--- Back-end/models/userModel.js
+++ Back-end/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -6,5 +8,34 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+userSchema.statics.signup = async function (email, password) {
+  // validate email and password
+  if (!email || !password) {
+    throw Error("Email and password are required");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email is invalid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "Password is weak, it must have at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number and 1 symbol"
+    );
+  }
+
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("User already exists");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hashedPassword });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
