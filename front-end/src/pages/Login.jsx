35ad7fc3741b08102781a4c9a4865344de85cab5
Adham Aboelela <100962253+AdhamIsHere@users.noreply.github.com>
2024-07-03 22:35:21 +0300

    chore: Add useLogout hook for user logout functionality and useLogin

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login(email, password);
   };
   return (
     <form className="login" onSubmit={handleSubmit}>
@@ -29,7 +31,10 @@ const Login = () => {
           setPassword(e.target.value);
         }}
       />
-      <button type="submit">Login</button>
+      <button type="submit" disabled={isLoading}>
+        Login
+      </button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
