335fdf2945cecd217ce93d565e71a9bbbecefc75
Adham Aboelela <100962253+AdhamIsHere@users.noreply.github.com>
2024-07-12 21:15:51 +0300

    Update .gitignore to ignore .env file

--- Back-end/middleware/requireAuth.js
+++ Back-end/middleware/requireAuth.js
@@ -1,6 +1,10 @@
+const jwt = require("jsonwebtoken");
+
 const requireAuth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
     return res.status(401).json({ error: "You must be logged in" });
   }
+
+  const token = authorization.split(" ")[1];
 };
