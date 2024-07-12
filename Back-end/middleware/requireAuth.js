3e0a2542ba5729b51841174916b77f449cd89968
Adham Aboelela <100962253+AdhamIsHere@users.noreply.github.com>
2024-07-12 21:14:12 +0300

    Update .gitignore to ignore .env and added middleware folder

---
+++ Back-end/middleware/requireAuth.js
@@ -0,0 +1,6 @@
+const requireAuth = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ error: "You must be logged in" });
+  }
+};
