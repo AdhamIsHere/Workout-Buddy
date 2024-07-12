7007c84371d4140b54216b8b41f0ed177bb2a016
Adham Aboelela <100962253+AdhamIsHere@users.noreply.github.com>
2024-07-12 21:27:58 +0300

    chore: Update requireAuth middleware to include user ID in request

--- Back-end/middleware/requireAuth.js
+++ Back-end/middleware/requireAuth.js
@@ -1,10 +1,21 @@
 const jwt = require("jsonwebtoken");
-
-const requireAuth = (req, res, next) => {
+const User = require("../models/userModel");
+const requireAuth = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
     return res.status(401).json({ error: "You must be logged in" });
   }
 
   const token = authorization.split(" ")[1];
+
+  try {
+    const { _id } = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findOne({ _id }).select("_id");
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: "Not Authorized" });
+  }
 };
+
+module.exports = requireAuth;
